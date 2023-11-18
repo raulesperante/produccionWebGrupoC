ff34c4f84b08ca8feda8d4158379cc25fc18023e
Raúl Esperante <ragusmat@hotmail.com>
2023-11-18 16:53:04 -0300

    Actualizo readme y migraciones tabla productos

--- database/migrations/2023_11_15_164208_create_products_table.php
+++ database/migrations/2023_11_15_164208_create_products_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 70);
+            $table->text('description');
+            $table->float('price');
+            $table->integer('amount');
+            $table->boolean('is_visible')->default(true);
+            $table->text('image');
             $table->timestamps();
         });
     }
