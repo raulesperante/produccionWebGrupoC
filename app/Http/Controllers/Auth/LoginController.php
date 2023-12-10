24704051ffc103e6c622296f117822bdad215fa1
raulesperante <47903995+raulesperante@users.noreply.github.com>
2023-12-10 16:44:34 -0300

    Arreglos web.php (#7)
    * Guardo cambios
    
    * Guardo cambios
    
    * Listo registro
    
    * Refactoring rutas

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,40 +1,64 @@
 <?php
 
 namespace App\Http\Controllers\Auth;
-
+use App\Models\User;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
+    public function register(Request $request){
+       // dd("Pasa por aca");
+       $request->validate([
+           'name' => 'required|string|max:255',
+           'surname' => 'required|string|max:255',
+           'email' => 'required|string|email|unique:users|max:255',
+           'password' => 'required|string|max:255',
+       ], []);
 
-    use AuthenticatesUsers;
+       $user = new User();
+       $user->name = $request->name;
+       $user->surname = $request->surname;
+       $user->email = $request->email;
+       $user->password = Hash::make($request->password);
+       $user->save();
 
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    protected $redirectTo = RouteServiceProvider::HOME;
+       Auth::login($user);
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('guest')->except('logout');
+       return redirect(route("home"));
+
     }
+
+    public function login(Request $request){
+        // Validacion
+        $credentials = [
+            "email" => $request->email,
+            "password" => $request->password,
+        ];
+
+        $remember = ($request->has('remember') ? true : false);
+        
+        if(Auth::attempt($credentials, $remember)){
+            $request->session()->regenerate();
+            return redirect()->intended(route("privada"));
+        }
+        return redirect(route("login"));
+
+    }
+
+    public function logout(Request $request){
+        Auth::logout();
+        // Estudiar estos dos metodos
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect(route('login'));
+
+    }
+
+
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,70 +1,65 @@
 @extends('layouts.app-client');
 
 @section('resources')
-    @vite([
-        'resources/css/tucuenta.css',
-    ])
-    <style>
-        body{
-            background-image: url("{{asset('/assets/images/fondologin.jpg')}}");
-            background-repeat: no-repeat;
-            background-size: cover;
-        }
+@vite([
+'resources/css/tucuenta.css',
+])
+<style>
+  body {
+    background-image: url("{{asset('/assets/images/fondologin.jpg')}}");
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
 
-        .bg{
-            background-image: url("{{asset('/assets/images/loginok.webp')}}");
-            background-position: center center;
-            background-size: cover;
-        }
-    </style>
+  .bg {
+    background-image: url("{{asset('/assets/images/loginok.webp')}}");
+    background-position: center center;
+    background-size: cover;
+  }
+</style>
 @endsection
 
 @section('content')
-    <div class="container w-75 bg-black mt-5 rounded shadow" id="log">
-      <div class="row align-items-stretch">
-        <div
-          class="col bg d-none d-lg-block col-md-5 col-lg-5 col-xl-6 rounded"
-        ></div>
-        <div class="col p-5 rounded-end">
-          <div class="text-end">
-            <img
-              src="/assets/images/logoprincipal.jpeg"
-              width="200"
-              alt="imagen logo principal"
-            />
-          </div>
-
-          <h2 class="fw-bold text-center pt-5 mb-5">Registrate!!!</h2>
-
-          <!--LOGIN-->
-
-          <form action="#">
-            <div class="mb-4">
-              <label for="correo" class="form-label">Correo Electronico</label>
-              <input
-                type="email"
-                class="form-control"
-                id="correo"
-                name="correo"
-                required
-              />
-            </div>
-            <div class="mb-4">
-              <label for="password" class="form-label">Contraseña</label>
-              <input
-                type="password"
-                class="form-control"
-                id="password"
-                name="password"
-                required
-              />
-            </div>
-          </form>
-        </div>
+<div class="container w-75 bg-black mt-5 rounded shadow" id="log">
+  <div class="row align-items-stretch">
+    <div class="col bg d-none d-lg-block col-md-5 col-lg-5 col-xl-6 rounded"></div>
+    <div class="col p-5 rounded-end">
+      <div class="text-end">
+        <img src="/assets/images/logoprincipal.jpeg" width="200" alt="imagen logo principal" />
       </div>
+
+      <h2 class="fw-bold text-center pt-5 mb-5">Registrate!!!</h2>
+
+      <!--LOGIN-->
+
+      <form action="{{ route('auth.registerLogic') }}"  method="POST">
+         @csrf
+        <div class="mb-4">
+          <label for="name" class="form-label">Nombre</label>
+          <input type="name" class="form-control" id="name" name="name" value="{{ old('name') }}" required />
+        </div>
+        <div class="mb-4">
+          <label for="surname" class="form-label">Apellido</label>
+          <input type="surname" class="form-control" id="surname" name="surname" value="{{ old('surname') }}" required />
+        </div>
+        <div class="mb-4">
+          <label for="email" class="form-label">Correo Electronico</label>
+          <input type="email" class="form-control" id="email" name="email" value="{{ old('email') }}" required />
+        </div>
+        <div class="mb-4">
+          <label for="password" class="form-label">Contraseña</label>
+          <input type="password" class="form-control" id="password" name="password" value="{{ old('password') }}" required />
+        </div>
+
+        <div >
+          <div class="d-grid text-center pt-2">
+            <button type="submit" class="btn btn-dark">Iniciar Sesión
+          </div>
+        </div>
     </div>
+    </form>
+  </div>
+</div>
+</div>
 
 @endsection
-  
-
-
