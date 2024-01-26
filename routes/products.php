13bbcc3d9b2529e0ace17e192b7b15023b1b02b0
Raúl Esperante <ragusmat@hotmail.com>
2024-01-26 09:42:14 -0300

    Fixed bug productos

--- routes/products.php
+++ routes/products.php
@@ -8,6 +8,7 @@ Route::controller(ProductController::class)->group(function(){
     Route::get('/productos/crear',"create")->name('products.create');
     Route::post('/productos/crear',"store")->name('products.store');
     Route::get('/productos/{product}',"show")->name('products.show');
-    Route::get('/productos/editar',"edit")->name('products.edit');
+    Route::get('/productos/editar/{product}',"edit")->name('products.edit');
+    Route::post('/productos/actualizar/{product}',"update")->name('products.update');
     Route::get('/productos/eliminar',"destroy")->name('products.destroy');
 });
