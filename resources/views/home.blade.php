a92fff7aa8a2d9249ee15d524bec440c9b5b2dcc
Raúl Esperante <ragusmat@hotmail.com>
2024-02-04 19:57:27 -0300

    Arreglo estilos home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,36 +1,50 @@
 @extends('layouts.app-client')
 
+@section('resources')
+@vite([
+'resources/css/inicio.css',
+])
+<style>
+  body {
+    background-image: url("{{asset('/assets/images/carrusel1.jpg')}}");
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+</style>
+@endsection
+
+<div style="margin-top: 3em"></div>
+
 @section('content')
-
-
-    <div class="text-container">
+    <div class="text-container" style="margin-top: 5em">
       <div class="container-fluid text-white">
         <h1>HACE CLICK EN LA COLECCION QUE QUIERAS VER...!!!</h1>
       </div>
     </div>
 
-    <!--- IMAGENES DE LA COLECCION DE ROPA -->
-    <div class="img-content mb-5 mt-5">
-      <div class="row container-fluid">
-        <div class="col-lg-6 col-md-6 col-sm-6">
+    <div class="row">
+      <div class="row mb-5  ">
+        <div class="col-6 text-center" >
           <a href="street_flashback.html">
             <img
+              style="max-height: 30em"
               class="img-fluid"
               src="assets/images/logoflashbacks.jpeg"
               alt="colleccionflashback"
             />
           </a>
         </div>
-        <div class="col-lg-6 col-md-6 col-sm-6">
+        <div class="col-6 text-center">
           <a href="street_spirit.html">
             <img
+              style="max-height: 30em"
               class="img-fluid"
               src="assets/images/logospirit.jpeg"
               alt="colleccionspirit"
             />
           </a>
         </div>
-      </div>
     </div>
+
 </body>
 @endsection
