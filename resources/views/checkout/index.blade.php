2c880a7885cfa619af9e5ce324a11d7b88626d00
Raúl Esperante <ragusmat@hotmail.com>
2024-02-25 18:57:34 -0300

    Guardo cambios

--- resources/views/checkout/index.blade.php
+++ resources/views/checkout/index.blade.php
@@ -12,7 +12,12 @@
     }
 
     #opt-shipping {
+        display: block;
+    }
+
+    #shipping-cost{
         display: none;
+
     }
 </style>
 @endsection
@@ -61,54 +66,52 @@
                                 </div>
                             </div>
                             <div id="opt-shipping" class="col-12 mt-2">
-                                <form action="">
-                                    <div class="row">
-                                        <div class="col-12">
-                                            <div class="input-group mb-3">
-                                                <input type="text" class="form-control" placeholder="Dirección"
-                                                    aria-label="Dirección" aria-describedby="basic-addon1"
-                                                    name="address">
-                                            </div>
+                                <div class="row">
+                                    <div class="col-12">
+                                        <div class="input-group mb-3">
+                                            <input type="text" class="form-control" placeholder="Dirección"
+                                                aria-label="Dirección" aria-describedby="basic-addon1" name="address">
                                         </div>
-                                        <div class="col-12 col-sm-6">
-                                            <div class="input-group mb-3">
-                                                <input min="1000" max="9000" type="number" class="form-control"
-                                                    placeholder="Código postal" aria-label="Código postal"
-                                                    aria-describedby="basic-addon1" name="postalCode">
-                                            </div>
-                                        </div>
-                                        <div class="col-12 col-sm-6">
-                                            <div class="input-group mb-3">
-                                                <select class="form-select" aria-label="Provincia">
-                                                    <option selected disabled>Provincia</option>
-                                                    <option value="1">One</option>
-                                                    <option value="2">Two</option>
-                                                    <option value="3">Three</option>
-                                                </select>
-                                            </div>
-                                        </div>
-                                        <div class="col-3">
-                                            <div class="input-group mb-3">
-                                                <input type="number" class="form-control" placeholder="Código"
-                                                    aria-label="Dirección" aria-describedby="basic-addon1"
-                                                    name="address">
-                                            </div>
-                                        </div>
-                                        <div class="col-9">
-                                            <div class="input-group mb-3">
-                                                <input type="number" class="form-control" placeholder="Teléfono"
-                                                    aria-label="Teléfono" aria-describedby="basic-addon1"
-                                                    name="address">
-                                            </div>
-                                        </div>
-
-                                        <button type="submit" class="btn button-primary">
-                                            Calcular costo de envío
-                                        </button>
-
-
                                     </div>
-                                </form>
+                                    <div class="col-12 col-sm-6">
+                                        <div class="input-group mb-3">
+                                            <input min="1000" max="9000" type="number" class="form-control"
+                                                placeholder="Código postal" aria-label="Código postal"
+                                                aria-describedby="basic-addon1" name="postalCode">
+                                        </div>
+                                    </div>
+                                    <div class="col-12 col-sm-6">
+                                        <div class="input-group mb-3">
+                                            <select class="form-select" aria-label="Provincia">
+                                                <option selected disabled>Provincia</option>
+                                                <option value="1">One</option>
+                                                <option value="2">Two</option>
+                                                <option value="3">Three</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-3">
+                                        <div class="input-group mb-3">
+                                            <input type="number" class="form-control" placeholder="Código"
+                                                aria-label="Dirección" aria-describedby="basic-addon1" name="address">
+                                        </div>
+                                    </div>
+                                    <div class="col-9">
+                                        <div class="input-group mb-3">
+                                            <input type="number" class="form-control" placeholder="Teléfono"
+                                                aria-label="Teléfono" aria-describedby="basic-addon1" name="address">
+                                        </div>
+                                    </div>
+
+                                    <button onclick="calculateShippingCost()"  class="btn button-primary">
+                                        Calcular costo de envío
+                                    </button>
+                                    <div id="shipping-cost" class="col-12 shippingCost">
+                                        $1500
+                                    </div>
+
+
+                                </div>
 
 
 
@@ -141,6 +144,11 @@
         }else{
           document.querySelector("#opt-shipping").style.display="none"
         }
+    }
+
+    function calculateShippingCost(){
+        document.querySelector("#shipping-cost").innerHTML = "$20009"
+        document.querySelector("#shipping-cost").style.display="block"
     }
 
 </script>
