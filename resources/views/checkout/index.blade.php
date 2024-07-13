ead33fa721ece79f9c06a1f9d2744db30b446079
Raúl Esperante <ragusmat@hotmail.com>
2024-07-13 20:07:21 -0300

    Simplificación checkout

--- resources/views/checkout/index.blade.php
+++ resources/views/checkout/index.blade.php
@@ -60,127 +60,45 @@
                         <div class="row ">
                             <div class="col-12">
                                 <div class="form-check ">
-                                    <input class="form-check-input" type="radio" name="flexRadioDefault"
-                                        id="flexRadioDefault1" checked onclick="showOptShipping(false)">
+                                    <input class="form-check-input" type="radio" name="shipping" value="local"
+                                        id="flexRadioDefault1" checked>
                                     <label class="form-check-label" for="flexRadioDefault1">
                                         Retiro en Local
                                     </label>
                                 </div>
                                 <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="flexRadioDefault"
-                                        id="flexRadioDefault2" onclick="showOptShipping(true)">
+                                    <input class="form-check-input" type="radio" name="shipping" value="home"
+                                        id="flexRadioDefault2">
                                     <label class="form-check-label" for="flexRadioDefault2">
                                         Envío a domicilio
                                     </label>
+                                    <br/>
+                                    <div style="font-size: 11px">Acordar entrega por mensajería interna al finalizar la compra</div>
                                 </div>
-                            </div>
-                            <div id="opt-shipping" class="col-12 col-md-10 mt-2">
-                                <div class="row">
-                                    <div class="col-12">
-                                        <div class="input-group mb-3">
-                                            <input type="text" class="form-control" placeholder="Dirección"
-                                                aria-label="Dirección" aria-describedby="basic-addon1" name="address">
-                                        </div>
-                                    </div>
-                                    <div class="col-12 col-sm-6">
-                                        <div class="input-group ">
-                                            <input id="postalCode" min="1000" max="9000" type="number"
-                                                class="form-control" placeholder="Código postal"
-                                                aria-label="Código postal" aria-describedby="basic-addon1"
-                                                name="postalCode">
-                                        </div>
-                                        <div id="msgErrorPostalCode" class="text-danger">El código postal debe estar
-                                            entre estos dos valores: 1000 y 9000</div>
-                                    </div>
-                                    <div class="col-12 col-sm-6 mt-3  mt-md-0">
-                                        <div class="input-group mb-3">
-                                            <select class="form-select" aria-label="Provincia">
-                                                <option selected disabled>Provincia</option>
-                                                <option value="1">One</option>
-                                                <option value="2">Two</option>
-                                                <option value="3">Three</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                    <div class="col-3">
-                                        <div class="input-group mb-3">
-                                            <input type="number" class="form-control" placeholder="Código"
-                                                aria-label="Dirección" aria-describedby="basic-addon1" name="address">
-                                        </div>
-                                    </div>
-                                    <div class="col-9">
-                                        <div class="input-group mb-3">
-                                            <input type="number" class="form-control" placeholder="Teléfono"
-                                                aria-label="Teléfono" aria-describedby="basic-addon1" name="address">
-                                        </div>
-                                    </div>
-
-                                    <button disabled onclick="calculateShippingCost()" class="btn button-primary"
-                                        id="shippingCostButton">
-                                        Calcular costo de envío
-                                    </button>
-                                    <div id="shipping-cost" class="col-12 shippingCost">
-                                    </div>
-
-
-                                </div>
-
-
-
                             </div>
                         </div>
                     </div>
                     <div class="col-12 col-sm-6 ">
                             <div class="form-check">
-                                <input class="form-check-input" type="radio" name="flexRadioDefault" id="cash" checked>
+                                <input class="form-check-input" type="radio" name="paymentMethod"  value="cash" id="cash" checked>
                                 <label class="form-check-label" for="cash">
                                     Efectivo en la tienda
                                 </label>
                             </div>
                             <div class="form-check">
-                                <input class="form-check-input" type="radio" name="flexRadioDefault" id="card">
+                                <input class="form-check-input" type="radio" name="paymentMethod" value="card" id="card">
                                 <label class="form-check-label" for="card">
-                                    Tarjeta de Crédito o Débito
+                                    Transferencia Bancaria
                                 </label>
+                            </div>
+                            <div class="row mt-2">
+                                <img style="height: 250px; width: 400px" src="/assets/images/cbu_dummy.png" alt="marca" class="img-fluid" />
                             </div>
                             <div class="row mt-2">
 
                                 <div class="col-12 col-md-8">
-
-                        <div style="background: #eee" class="p-2" >
-                                    <div class="row">
-                                        <div class="col-12">
-                                            <label for="cardNumber" class="form-label">Número de Tarjeta</label>
-                                            <div class="input-group ">
-                                                <input id="cardNumber" min="13" max="18" type="number"
-                                                    class="form-control" placeholder="XXXXXXXXXXXX"
-                                                    aria-label="Número de Tarjeta" aria-describedby="Número de Tarjeta">
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="row mt-1">
-                                        <div class="col-6">
-                                            <label for="cardExpiration" class="form-label">Caducidad (dd/mm)</label>
-                                            <div class="input-group ">
-                                                <input pattern="\d{2}/\d{2}" title="Formato válido: dd/mm"
-                                                    id="cardExpiration" type="text" class="form-control"
-                                                    placeholder="XXXX" aria-label="Caducidad de Tarjeta"
-                                                    aria-describedby="Caducidad de Tarjeta">
-                                            </div>
-
-                                        </div>
-                                        <div class="col-6">
-                                            <label for="cardCode" class="form-label">Código</label>
-                                            <div class="input-group ">
-                                                <input title="Introduzca tres dígitos" pattern="\d{3}" id="cardCode"
-                                                    type="text" class="form-control" placeholder="XXX"
-                                                    aria-label="Código de Tarjeta" aria-describedby="Código de Tarjeta">
-                                            </div>
-                                        </div>
-                                    </div>
-                        </div>
                                     <div class="col-12">
-                                        <button class="mt-3 btn button-primary w-100">Finalizar Compra</button>
+                                        <button class="mt-3 btn button-finalize-purchase w-100"><b>FINALIZAR COMPRA<b/></button>
                                     </div>
                             </div>
                         </div>
@@ -195,63 +113,9 @@
 
     </div>
 
-
-
     </div>
     </div>
 
 </section>
 </body>
 @endsection
-<script>
-    window.addEventListener('load', function() {
-        document.querySelector("#postalCode").addEventListener("input", function(e){
-            if(e.target.value){
-                document.getElementById('shippingCostButton').disabled = false;
-                const value = Number(e.target.value);
-                // Value ok
-                if(value >= 1000 && value <= 9000){
-                    document.getElementById('msgErrorPostalCode').style.display = "none";
-                }else{
-                    document.getElementById('msgErrorPostalCode').style.display = "block";
-                    document.getElementById('shippingCostButton').disabled = true;
-                }
-            }else{
-                document.getElementById('shippingCostButton').disabled = true;
-
-            }
-            document.getElementById('shipping-cost').innerText = "";
-            document.querySelector("#shipping-cost").style.display="none"
-        })
-    });
-
-    function showOptShipping(flag){
-        if(flag){
-          document.querySelector("#opt-shipping").style.display="block"
-        }else{
-          document.querySelector("#opt-shipping").style.display="none"
-        }
-    }
-
-    function calculateShippingCost(){
-        const token = document.querySelector('meta[name="csrf-token"]').content;
-        const value = Number(document.querySelector("#postalCode").value)
-        fetch('calculateShippingCost', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-CSRF-TOKEN': token
-            },
-            body: JSON.stringify({postalCode: value}),
-        })
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById('shipping-cost').innerText = "$" + data.cost;
-            document.querySelector("#shipping-cost").style.display="block"
-        })
-        .catch(error => {
-            console.error('Error al obtener datos:', error);
-        });
-    }
-
-</script>
