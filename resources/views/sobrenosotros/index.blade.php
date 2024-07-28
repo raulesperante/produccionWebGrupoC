f217f9646c89a3806f34e2d617a0d64f8987d3b5
Matias <matias.paulon@davinci.edu.ar>
2024-07-28 16:54:38 -0300

    Merge branch 'main' of https://github.com/raulesperante/produccionWebGrupoC into matias

---
+++ resources/views/sobrenosotros/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h1>Sobre Nosotros</h1>
+    <p>Bienvenidos a nuestra sección Sobre Nosotros.</p>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\FlashbackController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SpiritController;
+use App\Http\Controllers\EmpresaInfoController;
 
 include(base_path("routes/products.php"));
 include(base_path("routes/auth.php"));
@@ -17,5 +18,9 @@ Route::get('/', [HomeController::class, "index"])->name('home.index');
 Route::get('/spirit', [SpiritController::class, "index"])->name('spirit.index');
 Route::get('/flashback', [FlashbackController::class, "index"])->name('flashback.index');
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/sobrenosotros', [EmpresaInfoController::class, "index"])->name('sobrenosotros.index');
+//Route::get('/sobre/create', [EmpresaInfoController::class, 'create'])->name('sobre.create');
+//Route::post('/sobre', [EmpresaInfoController::class, 'store'])->name('sobre.store');
+
 
 
